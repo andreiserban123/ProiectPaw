123bb522f7cfeeb49ccddbcfe94f285794ac79f6
SERBAN EC ANDREI <andyspeed2003@gmail.com>
2024-03-19 16:34:21 +0200

    first

---
+++ ProiectPaw/Grup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace proiectPAW {
+    public class Grup {
+        
+        private List<string> drepturi;
+        private List<Utilizator> listaUtilizatori;
+
+        public Grup() {
+            drepturi = new List<string>();
+            listaUtilizatori = new List<Utilizator>();
+        }
+
+        public List<string> GetDrepturi() {
+            return new List<string>(drepturi);
+        }
+
+        public void SetDrepturi(List<string> drepturi) {
+            this.drepturi = new List<string>(drepturi);
+        }
+
+        public List<Utilizator> GetListaUtilizatori() {
+            List<Utilizator> copieUtilizatori = new List<Utilizator>();
+            foreach (Utilizator utilizator in listaUtilizatori) {
+                copieUtilizatori.Add(new Utilizator(utilizator.Nume, utilizator.Email, utilizator.Password, utilizator.CNP));
+            }
+            return copieUtilizatori;
+        }
+
+        public void SetListaUtilizatori(List<Utilizator> listaUtilizatori) {
+            this.listaUtilizatori = new List<Utilizator>();
+            foreach (Utilizator utilizator in listaUtilizatori) {
+                this.listaUtilizatori.Add(new Utilizator(utilizator.Nume, utilizator.Email, utilizator.Password, utilizator.CNP));
+            }
+        }
+    }
+}
---
+++ ProiectPaw/Utilizator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proiectPAW {
+    public class Utilizator {
+        private string nume;
+        private string email;
+        private string password;
+        private string cnp;
+        private DateTime dataNastere;
+
+        public Utilizator() { }
+        public Utilizator(string nume, string email, string password, string cnp) {
+            this.nume = nume;
+            this.email = email;
+            this.password = password;
+            this.cnp = cnp;
+        }
+
+        public string Nume {
+            get { return nume; }
+            set { nume = value; }
+        }
+
+        public string Email {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Password {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string CNP {
+            get { return cnp; }
+            set { cnp = value; }
+        }
+    }
+}
